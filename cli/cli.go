1c506c7ae941c8dfb10e7865404e1bc708975b96
João Fernandes <joaofnds@joaofnds.com>
2019-05-18 12:55:27 -0300

    Add cli package
    Initially this package provided functions to parse data provided as cli
    args

---
+++ cli/cli.go
@@ -0,0 +1,89 @@
+package cli
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+
+	"github.com/joaofnds/cache-sim/cache"
+	"github.com/joaofnds/cache-sim/file"
+)
+
+var (
+	cliConfigRegexp = regexp.MustCompile(`^(\d+):(\d+):(\d)$`)
+)
+
+const (
+	// GenerateData ...
+	GenerateData = iota
+	// NormalExecution ...
+	NormalExecution
+)
+
+var (
+	// ErrBadArgNum is the error returned when the number of arguments provided to the cli is wrong
+	ErrBadArgNum = fmt.Errorf("wrong number of arguments. Must be 3")
+	// ErrBadCacheFormat is the error return when the cache format provided is wrong
+	ErrBadCacheFormat = fmt.Errorf("bad cache format. format is <nsets>:<bsize>:<assoc>")
+)
+
+// Operation returns the operator to be performed by the program
+func Operation() int {
+	generate := flag.Bool("generate", false, "generate input data and exit")
+	flag.Parse()
+
+	if *generate {
+		return GenerateData
+	}
+
+	return NormalExecution
+}
+
+// ParseNormalExecArgs parses command line args for the normal program execution
+func ParseNormalExecArgs(args []string) (*cache.Cache, []int32, error) {
+	var c *cache.Cache
+	var references []int32
+	if len(args) != 3 {
+		return c, references, ErrBadArgNum
+	}
+
+	sets, blockSize, assoc, err := parseCacheConfig(args[1])
+	if err != nil {
+		return c, references, err
+	}
+
+	fileName := args[2]
+	c = cache.BuildCache(sets, blockSize, assoc)
+	f, err := os.Open(fileName)
+	if err != nil {
+		return c, references, err
+	}
+	defer f.Close()
+
+	references, err = file.ParseInputFile(f)
+	if err != nil {
+		return c, references, err
+	}
+
+	return c, references, nil
+}
+
+// parseCacheConfig parses the cache config provided via command line
+func parseCacheConfig(s string) (sets, blockSize, assoc int, err error) {
+	matched := cliConfigRegexp.FindAllStringSubmatch(s, -1)
+	if matched == nil || len(matched[0]) != 4 {
+		err = ErrBadArgNum
+		return
+	}
+
+	for k, v := range map[int]*int{1: &sets, 2: &blockSize, 3: &assoc} {
+		*v, err = strconv.Atoi(matched[0][k])
+		if err != nil {
+			err = ErrBadCacheFormat
+			return
+		}
+	}
+	return
+}
